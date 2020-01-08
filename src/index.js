44a7572a4426e6b79389b9773d572496d8bcee55
Josh Davies <jdaviescreative@gmail.com>
2020-01-08 17:32:48 +0000

    functional component practice

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function MyInfo() {
+  return (
+    <div>
+      <h1>Josh Davies</h1>
+      <p>learning react.js</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<MyInfo/>, document.getElementById("root"))
