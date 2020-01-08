dadec57f0849f6db15a76d4e7ee35170de47dbb5
Josh Davies <jdaviescreative@gmail.com>
2020-01-08 18:43:23 +0000

    created app basic structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import MyInfo from './components/MyInfo';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          save to reload.
-        </p>
-      </header>
+      <Header />
+      <MyInfo />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MyInfo from './components/MyInfo';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(<MyInfo />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'));
