7e20a3a544ab49e20943cfc94c95af851384ae76
Josh Davies <jdaviescreative@gmail.com>
2020-01-08 17:53:22 +0000

    created seperate file for react component

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Josh Davies</h1>
+      <p>learning react.js</p>
+      <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -2,21 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MyInfo from './components/MyInfo';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Josh Davies</h1>
-      <p>learning react.js</p>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  )
-}
-
-ReactDOM.render(<MyInfo/>, document.getElementById("root"))
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
