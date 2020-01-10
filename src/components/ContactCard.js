1487cd665b311149e425f413a746aee72302f586
Josh Davies <jdaviescreative@gmail.com>
2020-01-10 18:06:55 +0000

    playing with react components

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,8 +4,8 @@ function ContactCard(props) {
   return (
     <div className="contact-card">
       <img src={props.contact.imgUrl} />
-      <h3 style={{display: props.contact.name ? "block" : "none"}>Name: {props.contact.name}</h3>
-      <p>phone: {props.contact.phone}</p>
+      <h3 style={{display: props.contact.name ? "block" : "none"}}>Name: {props.contact.name}</h3>
+      <p style={{display: !props.question && "none"}}>phone: {props.contact.phone}</p>
       <p>email: {props.contact.email}</p>
     </div>
   )
